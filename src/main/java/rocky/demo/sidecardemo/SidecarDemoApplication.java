e067497b31ad3e45f19e05c9aad7196b7fcb15f7
Rocky <rocky341@protonmail.com>
2022-03-01 12:11:32 +0000

    commit empty springboot project

---
+++ src/main/java/rocky/demo/sidecardemo/SidecarDemoApplication.java
@@ -0,0 +1,13 @@
+package rocky.demo.sidecardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidecarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocky/demo/sidecardemo/SidecarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rocky.demo.sidecardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidecarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
